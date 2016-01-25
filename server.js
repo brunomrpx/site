ef83f71f4326629217ff6d36d27c4c9c70ea180c
Bruno Paixao <bruno@absoluta.net>
2016-01-25 18:01:36 -0200

    Added nodejs for serve static files

---
+++ server.js
@@ -0,0 +1,18 @@
+var finalhandler = require('finalhandler')
+var http = require('http')
+var serveStatic = require('serve-static');
+var port = 3000;
+
+// Serve up public/ftp folder
+var serve = serveStatic('./public', {'index': ['index.html', 'index.htm']})
+
+// Create server
+var server = http.createServer(function(req, res){
+  var done = finalhandler(req, res)
+  serve(req, res, done)
+})
+
+// Listen
+server.listen(port)
+
+console.log('Server started on localhost:' + port);
